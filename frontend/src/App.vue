84fe95e80f14c014842c93eba523d2220d1c2b1a
Gagle637 <gagle637@gmail.com>
2020-08-22 00:20:08 +0900

    Update Chart js | data

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <button @click="stock()">d3</button>
+    <img alt="Vue logo" src="@/assets/logo.png">
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-// import dataset from "./samsung.csv";
-var d3 = require("d3");
 
 export default {
   name: 'App',
-  methods: {
-    async stock() {
-      let data = await d3.csv("/dataset/samsung.csv");
-      console.log(data)
-    }
-  }
+
 }
 </script>
 
--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,12 +1,60 @@
 <template>
   <div>
     <h1>Hello World ! ! !</h1>
+    <button @click="stock()">d3</button>
+    <canvas id="chart"></canvas>
   </div>
 </template>
 
 <script>
+  import Chart from 'chart.js';
+  var d3 = require("d3");
+
+
   export default {
-    
+    methods: {
+      async stock() {
+        let data = await d3.csv("/dataset/samsung.csv");
+        let labels = data.map(function(d) {return d.Date})
+        let stockData = data.map(function(d) {return d.Close})
+        
+        var ctx = document.getElementById('chart')
+        var chart = new Chart(ctx, {
+          type: 'line',
+          options: {
+            legend: {
+              display: false
+            },
+            elements: {
+              point:{
+                radius: 2
+              }
+            },
+            scales: {
+              xAxes: [{
+                gridLines: {
+                  display:false
+                },
+                ticks: {
+                  display: false
+                }
+              }]
+            }
+          },
+          data: {
+            labels: labels,
+            datasets: [
+              {
+                fill: false,
+                borderColor: 'rgb(255, 99, 132)',
+                data: stockData
+              }
+            ]
+          }
+        })
+        chart
+      }
+    }
   }
 </script>
 
